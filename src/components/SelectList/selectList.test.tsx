454fc924ecd2ef38c56a13947984364ad8cda48a
Eliel Martins <elieuprata@gmail.com>
2024-10-27 16:12:52 -0300

    create test to SelectList component

---
+++ src/components/SelectList/selectList.test.tsx
@@ -0,0 +1,28 @@
+import { fireEvent, render, screen } from "@testing-library/react-native";
+
+import { SelectList } from "@components/SelectList";
+
+describe("Component: SelectList", () => {
+  it("should be return city details selected.", () => {
+    const data = [
+      { id: "1", name: "Novo Repartimento", latitude: 123, longitude: 456 },
+      { id: "2", name: "Campo Grande", latitude: 789, longitude: 987 },
+    ];
+
+    const onPress = jest.fn();
+
+    render(<SelectList data={data} onChange={() => {}} onPress={onPress} />);
+
+    const selectedCity = screen.getByText(/campo/i);
+    fireEvent.press(selectedCity);
+
+    expect(onPress).toHaveBeenCalledWith(data[1]);
+  });
+
+  it("not should be show when data props is empty.", () => {
+    render(<SelectList data={[]} onChange={() => {}} onPress={() => {}} />);
+
+    const options = screen.getByTestId("options");
+    expect(options.children).toHaveLength(0);
+  });
+});
