7f40bea4fbb7d607b79a4f258d87bf334631ec21
Eliel Martins <elieuprata@gmail.com>
2024-10-27 21:33:37 -0300

    create test to day component

---
+++ src/components/Day/day.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react-native";
+
+import { Day } from "@components/Day";
+
+import clearDay from "@assets/clear_day.svg";
+
+describe("Component: Day", () => {
+  it("should be render day.", () => {
+    render(
+      <Day
+        data={{
+          day: "18/07",
+          min: "30ºc",
+          max: "35ºc",
+          icon: clearDay,
+          weather: "Céu limpo",
+        }}
+      />
+    );
+
+    expect(screen.getByText("19/07")).toBeTruthy();
+  });
+});
